0cad6b85b6d48c2f020bd78de5fc48596cb6b36f
shweta281 <cse21310@glbitm.ac.in>
2024-11-16 20:02:23 +0530

    .

--- server.js
+++ server.js
@@ -10,13 +10,19 @@ const carRoutes = require("./routes/carRoutes");
 
 const app = express();
 
-const corsOptions = {
-  origin: "https://spyne-car-app-front-end-4meu.vercel.app", 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-};
-
-app.use(cors(corsOptions)); 
+// const corsOptions = {
+//   origin: "https://spyne-car-app-front-end-4meu.vercel.app", 
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+//   allowedHeaders: ['Content-Type', 'Authorization'], 
+// };
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://spyne-car-app-front-end-4meu.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
+app.options('*', cors(corsOptions));
+// app.use(cors(corsOptions)); 
 app.use(express.json());
 
 const swaggerOptions = {
